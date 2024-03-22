8c12bc6ae68e99d20421aaf15ebd39f39327c1c7
KatanaSword <114555620+KatanaSword@users.noreply.github.com>
2024-03-22 19:01:35 +0530

    portfolio backend setup

---
+++ backend/src/index.js
@@ -0,0 +1,21 @@
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+import { app } from "./app.js";
+
+dotenv.config({
+  path: "./.env",
+});
+
+connectDB()
+  .then(() => {
+    app.on("Error", (error) => {
+      console.log("Error", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 3500, () => {
+      console.log(`\n ⚙️  Server is running at port: ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection failed !!!", error);
+  });
