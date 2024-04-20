83620518b4505e81ef4f2ada6ab0b355b4051430
KatanaSword <114555620+KatanaSword@users.noreply.github.com>
2024-04-20 17:03:34 +0530

    Adding some helpers

---
+++ backend/src/utils/helpers.js
@@ -0,0 +1,35 @@
+import fs from "fs";
+
+export const getStaticFilePath = (req, fileName) => {
+  return `${req.protocal}://${req.get("host")}/images/${fileName}`;
+};
+
+export const getLocalPath = (fileName) => {
+  return `public/images/${fileName}`;
+};
+
+export const removeLocalFile = (localPath) => {
+  fs.unlink(localPath, (err) => {
+    if (err) {
+      console.log("Error while removing local files", err);
+    } else {
+      console.log("Removed local:", localPath);
+    }
+  });
+};
+
+export const getMongoosePaginationOptions = ({
+  page = 1,
+  limit = 10,
+  customLabels,
+}) => {
+  return {
+    page: Math.max(page, 1),
+    limit: Math.max(limit, 1),
+    pagination: true,
+    customLabels: {
+      pagingCounter: "serialNumberStartFrom",
+      ...customLabels,
+    },
+  };
+};
