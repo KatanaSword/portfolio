69e628a99c8390c809143f9701a6139ec0be969c
KatanaSword <114555620+KatanaSword@users.noreply.github.com>
2024-05-08 17:08:28 +0530

    Fix a bug to retrieve user from the DB using the decodedToken

--- backend/src/middlewares/auth.middlewares.js
+++ backend/src/middlewares/auth.middlewares.js
@@ -3,30 +3,29 @@ import { apiError } from "../utils/apiError.js";
 import { User } from "../models/user.models.js";
 import jwt from "jsonwebtoken";
 
-const verifyToken = asyncHandler(async (req, _, next) => {
+const verifyJWT = asyncHandler(async (req, _, next) => {
   try {
     const token =
       req.cookies?.accessToken ||
-      req.header("Authorization").replace("Bearer", "");
+      req.header("Authorization")?.replace("Bearer", "");
     if (!token) {
       throw new apiError(401, "Missing or invalid authentication token");
     }
 
-    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECURE);
-    if (!decodedToken) {
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    const user = await User.findOne(decodedToken?._id).select(
+      "-password -refreshToken"
+    );
+    if (!user) {
       throw new apiError(401, "Missing or invalid access token");
     }
-
-    const user = await User.findById(decodedToken?._id);
 
     req.user = user;
     next();
   } catch (error) {
-    throw new apiError(
-      401,
-      error?.message || "Missing or invalid access token"
-    );
+    throw new apiError(401, "Missing or invalid access token");
   }
 });
 
-export { verifyToken };
+export { verifyJWT };
