dc14a0ec86faa2168f4bbe074a9e21a3da553263
KatanaSword <114555620+KatanaSword@users.noreply.github.com>
2024-05-12 18:06:05 +0530

    Fix cors bug and refactor code

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,26 +1,27 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+
 const app = express();
 
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
-    credential: true,
+    credentials: true,
   })
 );
 
-app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.json({ limit: "18kb" }));
+app.use(express.urlencoded({ extended: true, limit: "18kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
 // import routes
 import userRoutes from "./routes/user.routes.js";
-import projectRoutes from "./routes/projects.routes.js";
+/* import projectRoutes from "./routes/projects.routes.js"; */
 
 // declare routes
-app.use("api/v1/users", userRoutes);
-app.use("api/v1/portfolio/projects", projectRoutes);
+app.use("/api/v1/users", userRoutes);
+/* app.use("api/v1/projects", projectRoutes); */
 
 export { app };
