5888b7e9ffb959c21ce148b8d8d844f1be7ebfe2
KatanaSword <114555620+KatanaSword@users.noreply.github.com>
2024-05-14 14:34:47 +0530

    Rename class

--- backend/src/middlewares/auth.middlewares.js
+++ backend/src/middlewares/auth.middlewares.js
@@ -1,5 +1,5 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { apiError } from "../utils/apiError.js";
+import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.models.js";
 import jwt from "jsonwebtoken";
 
@@ -9,7 +9,7 @@ const verifyJWT = asyncHandler(async (req, _, next) => {
       req.cookies?.accessToken ||
       req.header("Authorization")?.replace("Bearer", "");
     if (!token) {
-      throw new apiError(401, "Missing or invalid authentication token");
+      throw new ApiError(401, "Missing or invalid authentication token");
     }
 
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
@@ -18,13 +18,13 @@ const verifyJWT = asyncHandler(async (req, _, next) => {
       "-password -refreshToken"
     );
     if (!user) {
-      throw new apiError(401, "Missing or invalid access token");
+      throw new ApiError(401, "Missing or invalid access token");
     }
 
     req.user = user;
     next();
   } catch (error) {
-    throw new apiError(
+    throw new ApiError(
       401,
       error?.message || "Missing or invalid access token"
     );
