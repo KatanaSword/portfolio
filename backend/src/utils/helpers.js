0e71239bef7b8df7952e9d37a69d0f3b53cd62b1
KatanaSword <114555620+KatanaSword@users.noreply.github.com>
2024-05-15 16:16:35 +0530

    Remove image upload function

--- backend/src/utils/helpers.js
+++ backend/src/utils/helpers.js
@@ -1,23 +1,3 @@
-import fs from "fs";
-
-export const getStaticFilePath = (req, fileName) => {
-  return `${req.protocal}://${req.get("host")}/images/${fileName}`;
-};
-
-export const getLocalPath = (fileName) => {
-  return `public/images/${fileName}`;
-};
-
-export const removeLocalFile = (localPath) => {
-  fs.unlink(localPath, (err) => {
-    if (err) {
-      console.log("Error while removing local files", err);
-    } else {
-      console.log("Removed local:", localPath);
-    }
-  });
-};
-
 export const getMongoosePaginationOptions = ({
   page = 1,
   limit = 10,
